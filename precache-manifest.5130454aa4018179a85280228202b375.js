745f4b3517d183f48672165ea649f4e02ada970f
Gerson C Filho <gersoncfilho@mac.com>
2019-12-31 00:07:47 -0300

    Updates

---
+++ precache-manifest.5130454aa4018179a85280228202b375.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e1e908e5cd818a95b1266ecde33a789",
+    "url": "/indecision-app-new/index.html"
+  },
+  {
+    "revision": "7498fbc2c569ad138850",
+    "url": "/indecision-app-new/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "6c82eb9586c7a0713dd9",
+    "url": "/indecision-app-new/static/css/main.95d6d8d7.chunk.css"
+  },
+  {
+    "revision": "7498fbc2c569ad138850",
+    "url": "/indecision-app-new/static/js/2.12f86509.chunk.js"
+  },
+  {
+    "revision": "28ac78fef8675bfe412d94015e05d458",
+    "url": "/indecision-app-new/static/js/2.12f86509.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6c82eb9586c7a0713dd9",
+    "url": "/indecision-app-new/static/js/main.d4956d40.chunk.js"
+  },
+  {
+    "revision": "e8efd2087cc0b43f0df8",
+    "url": "/indecision-app-new/static/js/runtime-main.5c1eb79c.js"
+  }
+]);
